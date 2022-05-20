9e19979d5b39620d61925beb173e527dab69af7f
Artur Cesar <artur.cesar@redevistorias.com.br>
2022-05-20 16:49:49 -0300

    removing old codes

--- src/Budget.php
+++ src/Budget.php
@@ -2,39 +2,7 @@
 
 namespace App;
 
-use App\States\OnAproval;
-use App\States\State;
-
 class Budget
 {
-    public int $quantity;
-
     public float $value;
-
-    public State $state;
-
-    public function __construct()
-    {
-        $this->state = new OnAproval();
-    }
-
-    public function applyExtraDiscount()
-    {
-        $this->value -= $this->state->calcExtraDiscount($this);
-    }
-
-    public function approve()
-    {
-        $this->state->approve($this);
-    }
-
-    public function finalize()
-    {
-        $this->state->finalize($this);
-    }
-
-    public function reject()
-    {
-        $this->state->reject($this);
-    }
 }
